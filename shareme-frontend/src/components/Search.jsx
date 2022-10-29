26ea8ef7b5d77278926575d59e3ac3c8fe92a996
Adarsh Anand <adarsh.anand.20031@iitgoa.ac.in>
2022-10-30 00:11:12 +0530

    fixed inapt searches

--- shareme-frontend/src/components/Search.jsx
+++ shareme-frontend/src/components/Search.jsx
@@ -6,20 +6,52 @@ import { feedQuery, searchQuery } from "../utils/data";
 
 import Spinner from "./Spinner";
 import { Switch, Route, Redirect } from "react-router-dom";
+import { useToast } from "@chakra-ui/react";
+
+function isAlphabetorNumber(str) {
+  // return /^[a-zA-Z0-9]+$/.test(str);
+  let flag = true;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      !(
+        (str[i] >= "a" && str[i] <= "z") ||
+        (str[i] >= "A" && str[i] <= "Z") ||
+        (str[i] >= "0" && str[i] <= "9")
+      )
+    ) {
+      flag = false;
+    }
+  }
+  return flag;
+}
 
 const Search = ({ searchTerm }) => {
   const [pins, setPins] = useState();
   const [loading, setLoading] = useState(false);
+  const toast = useToast();
 
   useEffect(() => {
     if (searchTerm !== "") {
-      setLoading(true);
-      const query = searchQuery(searchTerm.toLowerCase()); // Get query
-      client.fetch(query).then((data) => {
-        // Fetch data from sanity
-        setPins(data);
-        setLoading(false);
-      });
+      if (isAlphabetorNumber(searchTerm)) {
+        setLoading(true);
+        const query = searchQuery(searchTerm.toLowerCase()); // Get query
+        client.fetch(query).then((data) => {
+          // Fetch data from sanity
+          setPins(data);
+          setLoading(false);
+        });
+      } else {
+        client.fetch(feedQuery).then((data) => {
+          setPins(data);
+          setLoading(false);
+        });
+        toast({
+          title: "Search term should be alphanumeric",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
     } else {
       client.fetch(feedQuery).then((data) => {
         setPins(data);
