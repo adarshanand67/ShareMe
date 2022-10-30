60fe95fd76f66827c930d85d4c0327bd5716a0dc
Adarsh Anand <adarsh.anand.20031@iitgoa.ac.in>
2022-10-30 21:56:01 +0530

    Added others category

--- shareme-frontend/src/utils/data.js
+++ shareme-frontend/src/utils/data.js
@@ -64,6 +64,7 @@ export const categories = [
     image:
       "https://i.pinimg.com/236x/2e/63/c8/2e63c82dfd49aca8dccf9de3f57e8588.jpg",
   },
+  
 ];
 
 export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
