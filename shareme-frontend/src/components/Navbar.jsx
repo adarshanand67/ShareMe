a0d056a7854a44421e2cd267cefc23344b11e139
Adarsh Anand <adarsh.anand.20031@iitgoa.ac.in>
2023-01-19 00:50:16 +0530

    Added close button

--- shareme-frontend/src/components/Navbar.jsx
+++ shareme-frontend/src/components/Navbar.jsx
@@ -1,6 +1,13 @@
 import PropTypes from "prop-types";
 import React from "react";
-import { IoMdAdd, IoMdMic, IoMdSearch } from "react-icons/io";
+// import vertical line from react icons
+import {
+  IoIosLink,
+  IoMdAdd,
+  IoMdClose,
+  IoMdMic,
+  IoMdSearch,
+} from "react-icons/io";
 import { Link, useNavigate } from "react-router-dom";
 import SpeechRecognition, {
   useSpeechRecognition,
@@ -14,6 +21,7 @@ const Navbar = ({ searchTerm, setSearchTerm }) => {
   const navigate = useNavigate();
   const user = fetchUser();
   // console.log(user);
+  
   const {
     transcript,
     listening,
@@ -34,33 +42,44 @@ const Navbar = ({ searchTerm, setSearchTerm }) => {
           <input
             type="text"
             onChange={(e) => setSearchTerm(e.target.value)}
-            placeholder="Search"
+            placeholder="Search Pins"
             value={searchTerm}
             onFocus={() => navigate("/search")}
+            onBlur={() => setSearchTerm("")} // Clear search term when user clicks outside
             className="p-2 w-full bg-gray-300 outline-none"
           />
-
-          {browserSupportsSpeechRecognition ? (
-            <button
-              className={`p-2 rounded-full ml-2 ${
-                listening ? MicActiveStyles : MicInactiveStyles
-              }`}
-              onClick={
-                listening
-                  ? resetTranscript && setSearchTerm(transcript)
-                  : () => {
-                      navigateToSearch();
-                      SpeechRecognition.startListening();
-                    }
-              }
-            >
-              <IoMdMic fontSize={21} />
-            </button>
-          ) : null}
-          {/* <p>{transcript}</p> */}
+          {/* Close button */}
+          <button
+            className="p-2 rounded-full ml-2 bg-gray-300 text-gray-500"
+            onClick={() => setSearchTerm("")}
+          >
+            <IoMdClose fontSize={21} />
+          </button>
         </div>
+
+        {/* Voice search */}
+
+        {browserSupportsSpeechRecognition ? (
+          <button
+            className={`p-4 rounded-full ml-2 hover:bg-red-400
+            ${
+              listening ? MicActiveStyles : MicInactiveStyles
+            }`}
+            onClick={
+              listening
+                ? resetTranscript && setSearchTerm(transcript)
+                : () => {
+                    navigateToSearch();
+                    SpeechRecognition.startListening();
+                  }
+            }
+          >
+            <IoMdMic fontSize={21} />
+          </button>
+        ) : null}
+
+        {/* User Profile */}
         <div className="flex gap-5 ">
-          {/* User Profile */}
           <Link to={`user/${user?.uid}`} className="hidden md:block">
             <img
               src={user.photoURL}
