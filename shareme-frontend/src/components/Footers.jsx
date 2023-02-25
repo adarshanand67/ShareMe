68ca27dcbfdf22beec8e97169dbab2b88c87a36f
Laxman Kohar <lkverma5422@gmail.com>
2023-02-26 01:09:25 +0530

    testing pre-commit

--- shareme-frontend/src/components/Footers.jsx
+++ shareme-frontend/src/components/Footers.jsx
@@ -1,24 +1,24 @@
-import { Footer } from "flowbite-react";
-import React, { useState } from "react";
-import { BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
-import SocialMediaButtons from "./SocialMediaButtons";
+import React, { useState } from 'react';
+import { Footer } from 'flowbite-react';
+import { BsFacebook, BsGithub, BsInstagram, BsTwitter } from 'react-icons/bs';
+import SocialMediaButtons from './SocialMediaButtons';
 
 const Footers = () => {
-  const url = window.location.href;
-  return (
-    <>
-      <div className="mt-2 w-full">
-        {/* <Footer.Divider /> */}
-        <h1 className="mb-2 text-center text-2xl">Help us spread the word!</h1>
-        <SocialMediaButtons url={url} text="ShareMe™ - Documenting your life" />
-        <div className="flex w-full justify-center pb-2">
-          <hr />
+	const url = window.location.href;
+	return (
+		<>
+			<div className="mt-2 w-full">
+				{/* <Footer.Divider /> */}
+				<h1 className="mb-2 text-center text-2xl">Help us spread the word!</h1>
+				<SocialMediaButtons url={url} text="ShareMe™ - Documenting your life" />
+				<div className="flex w-full justify-center pb-2">
+					<hr />
 
-          <Footer.Copyright href="#" by="ShareMe™" year={2023} />
-        </div>
-      </div>
-    </>
-  );
+					<Footer.Copyright href="#" by="ShareMe™" year={2023} />
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Footers;
